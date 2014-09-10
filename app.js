43305e7b19ff9f72a3bb5566d17f7c0720254507
Gorden <wangshiqing_wsq@126.com>
2014-09-10 14:56:55 +0800

    no message

--- app.js
+++ app.js
@@ -2,20 +2,30 @@ var http = require("http");
 var url = require("url");
 var qs = require("querystring");
 var util = require("./lib/util");
+var db = require("./lib/db");
 
 util = new util();
+
 var server = http.createServer(function (req, res) {
     var path = util.getPath(req);
     var param = util.getParam(req);
     var JSONP = util.getJSONPName(req);
     if (JSON.stringify(param) != "{}") {
         console.log(param);
+        /*
+
         util.router(param, function (resdata) {
 
             console.log(resdata);
-            res.writeHead(200, {"Content-Type": "text/html"})
+            res.writeHead(200, {"Content-Type": "text/html"});
             res.end(util.makeJSONP(JSONP, resdata))
         });
+
+        */
+
+        db = new db(req,res);
+
+
     }
 
 });
